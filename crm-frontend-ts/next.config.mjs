961210fd70e9b074e8e430fd026f336982d9a8ae
rohithuta-p <rohithutagonna@murallys.com>
2026-01-15 17:29:37 +0530

    deploy v1

---
+++ crm-frontend-ts/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",   
+  // basePath: "/frontend",  
+  // assetPrefix: "/frontend", 
+};
+
+export default nextConfig;
